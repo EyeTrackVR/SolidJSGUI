9d424412a4573173ce0b69c0eebaa59d8642b9d5
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2023-01-02 21:26:34 +0000

    fix formatting and vite config error

--- GUI/ETVR/vite.config.ts
+++ GUI/ETVR/vite.config.ts
@@ -3,24 +3,24 @@ import solid from 'solid-start/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  clearScreen: false,
-  resolve: {
-    alias: {
-      '@interfaces': path.resolve(__dirname, './src/interfaces'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@redux': path.resolve(__dirname, './src/redux'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@styles': path.resolve(__dirname, './src/styles'),
-      '@config': path.resolve(__dirname, './src/config'),
-      '@src': path.resolve(__dirname, './src'),
-      '@assets': path.resolve(__dirname, './assets'),
-      '@hooks': path.resolve(__dirname, './src/utils/hooks'),
-      '@static': path.resolve(__dirname, './src/static'),
-      '@utils': path.resolve(__dirname, './src/utils'),
+    clearScreen: false,
+    resolve: {
+        alias: {
+            '@interfaces': path.resolve(__dirname, './src/interfaces'),
+            '@components': path.resolve(__dirname, './src/components'),
+            '@redux': path.resolve(__dirname, './src/redux'),
+            '@pages': path.resolve(__dirname, './src/pages'),
+            '@styles': path.resolve(__dirname, './src/styles'),
+            '@config': path.resolve(__dirname, './src/config'),
+            '@src': path.resolve(__dirname, './src'),
+            '@assets': path.resolve(__dirname, './assets'),
+            '@hooks': path.resolve(__dirname, './src/utils/hooks'),
+            '@static': path.resolve(__dirname, './src/static'),
+            '@utils': path.resolve(__dirname, './src/utils'),
+        },
     },
-  },
-  plugins: [solid(ssr: false)],
-  ssr: {
-    noExternal: ['@hope-ui/core', '@hope-ui/styles'],
-  },
+    plugins: [solid({ ssr: false })],
+    ssr: {
+        noExternal: ['@hope-ui/core', '@hope-ui/styles'],
+    },
 })
