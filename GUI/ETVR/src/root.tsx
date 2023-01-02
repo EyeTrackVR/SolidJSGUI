87106fef2ab252e50e5bbefdd9938f24821800f3
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2023-01-02 22:19:03 +0000

    update - implement custom HTML logic to root

--- GUI/ETVR/src/root.tsx
+++ GUI/ETVR/src/root.tsx
@@ -14,7 +14,6 @@ export default function Root() {
                 <Meta charset="utf-8" />
                 <Meta name="viewport" content="width=device-width, initial-scale=1" />
                 <script type="text/partytown" src="./scripts/typetura.min.js" />
-                <script type="text/partytown" src="./scripts/custom.js" />
             </Head>
             <Body>
                 <ColorModeScript />
@@ -22,9 +21,39 @@ export default function Root() {
                     <Suspense>
                         <ErrorBoundary>
                             <AppRoutes />
+                            <div class="main-div">
+                                <div
+                                    data-tauri-drag-region
+                                    class="titlebar"
+                                    style={{
+                                        position: 'relative',
+                                        'z-index': 10,
+                                    }}>
+                                    <div class="titlebar-button" id="titlebar-minimize">
+                                        <img
+                                            src="https://api.iconify.design/mdi:window-minimize.svg"
+                                            alt="minimize"
+                                        />
+                                    </div>
+                                    <div class="titlebar-button" id="titlebar-maximize">
+                                        <img
+                                            src="https://api.iconify.design/mdi:window-maximize.svg"
+                                            alt="maximize"
+                                        />
+                                    </div>
+                                    <div class="titlebar-button" id="titlebar-close">
+                                        <img
+                                            src="https://api.iconify.design/mdi:close.svg"
+                                            alt="close"
+                                        />
+                                    </div>
+                                </div>
+                                <div id="root" />
+                            </div>
                         </ErrorBoundary>
                     </Suspense>
                 </HopeProvider>
+                <script type="text/partytown" src="./scripts/custom.js" />
                 <Scripts />
             </Body>
         </Html>
