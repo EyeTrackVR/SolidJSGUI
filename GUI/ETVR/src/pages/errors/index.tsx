4728ea16118b003d72c67bac6800292382f26f35
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2023-01-25 17:16:55 +0000

    style: 404 page

--- GUI/ETVR/src/pages/errors/index.tsx
+++ GUI/ETVR/src/pages/errors/index.tsx
@@ -1,8 +1,17 @@
 const page404 = () => {
     return (
-        <section class="text-gray-700 p-8">
-            <h1 class="text-2xl font-bold">404: Not Found</h1>
-            <p class="mt-4">Welp, something went wrong 😞</p>
+        <section
+            class="text-white flex flex-col items-center justify-center fixed h-[100%] p-8"
+            style={{
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                '-webkit-transform': 'translate(-50%, -50%)',
+            }}>
+            <div class="card rounded-md bg-slate-700">
+                <h1 class="text-2xl font-bold">404: Not Found</h1>
+                <p class="mt-4 text-gray-400 text-lg">Welp, something went wrong 😞</p>
+            </div>
         </section>
     )
 }
