37bfadd4eb015be6f6284e1c0d2cddab18cc2175
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2023-01-25 13:51:21 +0000

    refactor: clean up app.tsx

--- GUI/ETVR/src/app.tsx
+++ GUI/ETVR/src/app.tsx
@@ -4,37 +4,43 @@ import { appWindow } from '@tauri-apps/api/window'
 import { onMount, Suspense } from 'solid-js'
 import AppRoutes from './routes/Routes'
 
-const app = () => {
-    injectCriticalStyle()
-    onMount(() => {
-        /* invoke('toggle_decorations') */
-        document.documentElement.style.setProperty('--window-opacity', '0')
-        document.addEventListener('DOMContentLoaded', () => {
-            invoke('get_user')
-                .then((config) => {
-                    console.log(config)
-                    localStorage.setItem('settings', JSON.stringify(config))
-                })
-                .catch((e) => console.error(e))
-            setTimeout(() => invoke('close_splashscreen'), 500)
-        })
-        /* document.addEventListener('mousedown', clickOutside) */
-        const titlebar = document.getElementsByClassName('titlebar')
-        if (titlebar) {
-            /* titlebar.addEventListener('contextmenu', (e) => {
+const handleTitlebar = () => {
+    const titlebar = document.getElementsByClassName('titlebar')
+    if (titlebar) {
+        /* titlebar.addEventListener('contextmenu', (e) => {
                     e.preventDefault()
                     setState('menuOpen', { x: e.clientX, y: e.clientY })
                 }) */
-            document
-                .getElementById('titlebar-minimize')
-                ?.addEventListener('click', () => appWindow.minimize())
-            document
-                .getElementById('titlebar-maximize')
-                ?.addEventListener('click', () => appWindow.toggleMaximize())
-            document
-                .getElementById('titlebar-close')
-                ?.addEventListener('click', () => appWindow.close())
-        }
+        document
+            .getElementById('titlebar-minimize')
+            ?.addEventListener('click', () => appWindow.minimize())
+        document
+            .getElementById('titlebar-maximize')
+            ?.addEventListener('click', () => appWindow.toggleMaximize())
+        document
+            .getElementById('titlebar-close')
+            ?.addEventListener('click', () => appWindow.close())
+    }
+}
+
+const handleAppBoot = () => {
+    document.documentElement.style.setProperty('--window-opacity', '0')
+    document.addEventListener('DOMContentLoaded', () => {
+        invoke('get_user')
+            .then((config) => {
+                console.log(config)
+                localStorage.setItem('settings', JSON.stringify(config))
+            })
+            .catch((e) => console.error(e))
+        setTimeout(() => invoke('close_splashscreen'), 500)
+    })
+}
+
+const app = () => {
+    injectCriticalStyle()
+    onMount(() => {
+        handleTitlebar()
+        handleAppBoot()
     })
     return (
         <div class="App overflow-y-auto">
