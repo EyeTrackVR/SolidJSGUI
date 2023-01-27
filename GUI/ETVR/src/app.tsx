ace5315a81a7ee8db9fcf132a72d27bd8418f43e
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2023-01-27 22:06:51 +0000

    minor change to modal example

--- GUI/ETVR/src/app.tsx
+++ GUI/ETVR/src/app.tsx
@@ -59,10 +59,18 @@ const ModalContent = () => {
     return (
         <>
             <div class="mt-2">
-                <p class="text-sm text-gray-900 dark:text-gray-50">
-                    Your payment has been successfully submitted. We've sent your an email with all
-                    of the details of your order.
-                </p>
+                <div class="text-sm text-gray-900 dark:text-gray-50">
+                    This is a modal window. You can do the following things with it:
+                    <br />
+                    <ul class="mt-2 list-disc list-inside text-sm text-gray-600 dark:text-gray-400">
+                        <li>Read more information about a context</li>
+                        <li>Find similar contexts</li>
+                        <li>
+                            Close this modal by clicking the background overlay or pressing the
+                            button below
+                        </li>
+                    </ul>
+                </div>
             </div>
             <div class="mt-4">
                 <button type="button" class={BUTTON} onClick={() => setOpenModal(false)}>
