8978073373583faec2cd84f593583741dd77f578
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2023-01-30 15:19:49 +0000

    update: begin implementing notifications

--- GUI/ETVR/src/components/Notifications/index.tsx
+++ GUI/ETVR/src/components/Notifications/index.tsx
@@ -1,72 +1,16 @@
-import { Transition, useToaster, Toaster, Toast } from 'solid-headless'
+import { Transition, useToaster, Toaster, Alert } from 'solid-headless'
+import { IoAlertCircleSharp } from 'solid-icons/io'
 import { createEffect, createSignal, JSX, onCleanup, For } from 'solid-js'
+import CustomToast from './CustomToast'
+import CloseIcon from '@src/components/CloseIcon'
 import { notifications } from '@src/store/ui/selectors'
 
-interface ToastProps {
-    id: string
-    message: string
-}
-
-const CloseIcon = (props: JSX.IntrinsicElements['svg']) => {
-    return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            {...props}>
-            <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width={2}
-                d="M6 18L18 6M6 6l12 12"
-            />
-        </svg>
-    )
-}
-
-const CustomToast = (props: ToastProps) => {
-    const [isOpen, setIsOpen] = createSignal(true)
-
-    const dismiss = () => {
-        setIsOpen(false)
-    }
-
-    return (
-        <Transition
-            show={isOpen()}
-            class="relative transition rounded-lg p-4 bg-gray-900 border-2 border-gray-900 dark:border-gray-50"
-            enter="ease-out duration-300"
-            enterFrom="opacity-0 scale-50"
-            enterTo="opacity-100 scale-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100 scale-100"
-            leaveTo="opacity-0 scale-50"
-            afterLeave={() => {
-                notifications()?.remove(props.id)
-            }}>
-            <Toast class="flex justify-between items-center">
-                <span class="flex-1 text-sm font-semibold text-gray-50">{props.message}</span>
-                <button
-                    type="button"
-                    class="flex-none w-6 h-6 p-1 text-gray-50 rounded-full focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
-                    onClick={dismiss}>
-                    <CloseIcon />
-                </button>
-            </Toast>
-        </Transition>
-    )
-}
-
-const ToastNotificationWindow = () => {
+const ToastNotificationWindow: JSX.Element = () => {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     const notifs = useToaster(notifications()!)
 
     const [isOpen, setIsOpen] = createSignal(false)
-    let persist = true
-
     const closeNotifs = () => {
-        persist = false
         setIsOpen(false)
     }
 
@@ -78,19 +22,17 @@ const ToastNotificationWindow = () => {
         if (notifs().length > 0) {
             setIsOpen(true)
         }
-        if (persist) {
-            return
-        }
         const timeout = setTimeout(() => {
             closeNotifs()
-        }, 10000)
+            console.log('[Notifications] Closed - Cleaned up')
+        }, 5000)
         onCleanup(() => {
             clearTimeout(timeout)
         })
     })
 
     return (
-        <Toaster class="absolute fixed-0 right-0 bottom-0 z-10">
+        <Toaster class="absolute p-2 fixed-0 right-0 bottom-0 z-10">
             <Transition
                 show={isOpen()}
                 class="relative transition"
@@ -101,29 +43,23 @@ const ToastNotificationWindow = () => {
                 leaveFrom="opacity-100 scale-100 translate-y-0"
                 leaveTo="opacity-0 scale-50  translate-y-full"
                 afterLeave={clearNotifs}>
-                <div class="flex flex-col m-4 max-w-full max-h-[50vh] overflow-hidden rounded-xl shadow-xl bg-gray-50 dark:bg-gray-900 border border-gray-900 dark:border-gray-50 p-4 space-y-2">
-                    <div class="flex-none flex items-center justify-between">
-                        <span class="text-xl font-bold text-gray-900 dark:text-gray-50">
-                            Notifications
-                        </span>
-                        <button
-                            type="button"
-                            onClick={closeNotifs}
-                            class="w-6 h-6 p-1 text-gray-900 dark:text-gray-50 bg-gray-50 dark:bg-gray-900 rounded-full focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-                            <CloseIcon />
-                        </button>
-                    </div>
-                    <div class="flex-1 flex flex-col-reverse space-y-reverse space-y-1 overflow-y-auto overflow-x-hidden rounded-lg">
-                        <For
-                            each={notifs().slice(0).reverse()}
-                            fallback={
-                                <div class="bg-gray-900 dark:bg-gray-50 flex items-center justify-center text-bold text-gray-50 dark:text-gray-900 p-4">
-                                    You have no notifications.
-                                </div>
-                            }>
-                            {(item) => <CustomToast id={item.id} message={item.data} />}
-                        </For>
-                    </div>
+                <div class="flex-1 flex flex-col-reverse space-y-reverse space-y-1 overflow-y-auto overflow-x-hidden rounded-lg">
+                    <For
+                        each={notifs().slice(0).reverse()}
+                        fallback={
+                            <Alert class="bg-slate-600 flex grow flex-row items-center justify-center text-xl text-bold text-gray-50 p-4">
+                                <IoAlertCircleSharp size={25} color="#90CDF4" />
+                                <p class="pl-1 pr-1">no notifications</p>
+                                <button
+                                    type="button"
+                                    class="bg-rose-600 hover:bg-rose-700 focus:bg-rose-900 flex-none w-6 h-6 p-1 text-gray-50 rounded-full focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+                                    onClick={closeNotifs}>
+                                    <CloseIcon />
+                                </button>
+                            </Alert>
+                        }>
+                        {(item) => <CustomToast id={item.id} message={item.data} />}
+                    </For>
                 </div>
             </Transition>
         </Toaster>
--- GUI/ETVR/src/pages/Settings/index.tsx
+++ GUI/ETVR/src/pages/Settings/index.tsx
@@ -4,7 +4,13 @@ import { addNotification } from '@src/store/ui/ui'
 const SettingsPage = () => {
     return (
         <div class="pb-[5rem] h-[95%] xl:h-[100%] xl:pb-[1rem] grow flex-col pt-6 py-6 px-8">
-            <Button color="blue" onClick={() => addNotification('This is a test')} text="Add Notification" />
+            <Button
+                color="blue"
+                onClick={() =>
+                    addNotification(`This toast is created on ${new Date().toTimeString()}`)
+                }
+                text="Add Notification"
+            />
         </div>
     )
 }
