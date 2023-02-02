f812192edfeba054c5cae4e171c3545b0c83b147
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2023-02-02 14:25:34 +0000

    feat: added test for notifications - added dropdown menu to test notifiction type - upgraded kobalte-ui to most recent version

--- GUI/ETVR/src/pages/Settings/index.tsx
+++ GUI/ETVR/src/pages/Settings/index.tsx
@@ -1,10 +1,41 @@
+import { DropdownMenu } from '@kobalte/core'
+import { createSignal } from 'solid-js'
 import Button from '@components/Button'
 import { addNotification } from '@src/store/ui/ui'
 import { ENotificationAction } from '@utils/enums'
+import './styles.css'
 
 const SettingsPage = () => {
+    const [open, setOpen] = createSignal(false)
+    const [type, setType] = createSignal<ENotificationAction>(ENotificationAction.APP)
     return (
         <div class="pb-[5rem] h-[95%] xl:h-[100%] xl:pb-[1rem] grow flex-col pt-6 py-6 px-8">
+            <DropdownMenu.Root isOpen={open()} onOpenChange={setOpen}>
+                <DropdownMenu.Trigger class="dropdown-menu__trigger">
+                    <span>Notification Type</span>
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Portal>
+                    <DropdownMenu.Content class="dropdown-menu__content">
+                        <DropdownMenu.Group>
+                            <DropdownMenu.GroupLabel class="dropdown-menu__group-label">
+                                Type
+                            </DropdownMenu.GroupLabel>
+                            <DropdownMenu.Item
+                                class="dropdown-menu__item"
+                                onClick={() => setType(ENotificationAction.APP)}>
+                                App Level Notification
+                            </DropdownMenu.Item>
+                            <DropdownMenu.Separator class="dropdown-menu__separator" />
+                            <DropdownMenu.Item
+                                class="dropdown-menu__item"
+                                onClick={() => setType(ENotificationAction.OS)}>
+                                OS Level Notification
+                            </DropdownMenu.Item>
+                        </DropdownMenu.Group>
+                        <DropdownMenu.Arrow />
+                    </DropdownMenu.Content>
+                </DropdownMenu.Portal>
+            </DropdownMenu.Root>
             <Button
                 color="blue"
                 onClick={() =>
@@ -13,7 +44,7 @@ const SettingsPage = () => {
                             title: 'Test',
                             message: `This toast is created on ${new Date().toTimeString()}`,
                         },
-                        ENotificationAction.OS,
+                        type(),
                     )
                 }
                 text="Add Notification"
