8aeb86a7edbd3d169cb8683d73546af7ff02dcb7
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2023-02-05 15:53:09 +0000

    fix: fix linting issue

--- GUI/ETVR/src/components/WebSerial/index.tsx
+++ GUI/ETVR/src/components/WebSerial/index.tsx
@@ -1,9 +1,9 @@
-declare global {
+declare module 'solid-js' {
     // eslint-disable-next-line @typescript-eslint/no-namespace
     namespace JSX {
         interface IntrinsicElements {
             // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            'esp-web-install-button': any
+            'esp-web-install-button': any // replace any with the type of props
         }
     }
 }
@@ -18,7 +18,11 @@ const WebSerial = () => {
     return (
         <div>
             <esp-web-install-button overrides={checkSameFirmware} manifest="firmware/manifest.json">
-                <button class="rounded-[8px] bg-blue-700 p-2 text-white mt-1 hover:bg-blue-600 focus:bg-blue-500" slot="activate">Custom install button</button>
+                <button
+                    class="rounded-[8px] bg-blue-700 p-2 text-white mt-1 hover:bg-blue-600 focus:bg-blue-500"
+                    slot="activate">
+                    Custom install button
+                </button>
                 <span slot="unsupported">Ah snap, your browser doesn't work!</span>
                 <span slot="not-allowed">Ah snap, you are not allowed to use this on HTTP!</span>
             </esp-web-install-button>
