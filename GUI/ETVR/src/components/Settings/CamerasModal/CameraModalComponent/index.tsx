20eecf571032b62b491d65c42a424aea43f532bf
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2023-03-03 16:30:50 +0000

    fix: iframe instance bug

--- GUI/ETVR/src/components/Settings/CamerasModal/CameraModalComponent/index.tsx
+++ GUI/ETVR/src/components/Settings/CamerasModal/CameraModalComponent/index.tsx
@@ -1,3 +1,5 @@
+import WebSocketHandler from '@components/WebSocket'
+
 export interface IProps {
     cameraHeight: number
     cameraWidth?: number
@@ -7,14 +9,15 @@ export interface IProps {
 const CameraModalComponent = (props: IProps) => {
     return (
         <div>
-            <iframe
+            <WebSocketHandler borderRadius="rounded-[14px]" />
+            {/* <iframe
                 name="TV"
                 class="rounded-xl"
                 height={props.cameraHeight}
                 width={props.cameraWidth || '100%'}
                 src={props.cameraSrc}
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; picture-in-picture full"
-            />
+            /> */}
         </div>
     )
 }
