56d33d1a869757b2d6030144d9e6e79c730be4bf
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2023-03-10 19:28:30 +0000

    fix: add assetes and fix prepareCMD

--- repo-tools/scripts/prepareCMD.sh
+++ repo-tools/scripts/prepareCMD.sh
@@ -3,18 +3,30 @@
 # create a vairable to hold a passed in argument
 # this argument is the next release version
 # this is passed in from the .releaserc file
+
+sudo apt-get install -y jq
+
 nextReleaseVersion=$1
+TARGET_KEY="version"
+
+printf "Next version: ${nextReleaseVersion}\n"
 
 # This script is used to execute the prepareCMD.sh script on the remote host
 printf "Executing prepareCMD.sh on remote host \n"
 
 printf "Update the version in the package.json file \n"
-sed -i "s/\"version\": \"[0-9\\.]*\",/\"version\": \"\\$nextReleaseVersion\",/g" ./GUI/ETVR/package.json
+
+# make a temp file
+tmp=$(mktemp)
+
+jq --arg a "$nextReleaseVersion" '.version = $a' ./GUI/ETVR/package.json > "$tmp" && mv "$tmp" ./GUI/ETVR/package.json -f
 
 printf "Update the version in the tauri.conf.json file \n"
-sed -i "s/\"version\": \"[0-9\\.]*\",/\"version\": \"\\$nextReleaseVersion\",/g" ./GUI/ETVR/src-tauri/tauri.conf.json
+
+jq --arg a "$nextReleaseVersion" '.version = $a' ./GUI/ETVR/src-tauri/tauri.conf.json > "$tmp" && mv "$tmp" ./GUI/ETVR/src-tauri/tauri.conf.json -f
 
 printf "Update the version in the Cargo.toml file \n"
-sed -i "s/version = \"[0-9\\.]*\"/version = \"\\$nextReleaseVersion\"/g" ./GUI/ETVR/src-tauri/Cargo.toml
+
+sed -i "s/version = \"[0-9\\.]*\"/version = \"${nextReleaseVersion}\"/g" ./GUI/ETVR/src-tauri/Cargo.toml
 
 printf "[prepareCMD.sh]: Done \n"
