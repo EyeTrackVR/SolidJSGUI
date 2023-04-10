2f208c2d0654eeeecb369923c4dd33cab4df7ebc
luckmer <woszczo04@gmail.com>
2023-04-10 13:44:30 +0200

    new icons

--- GUI/ETVR/assets/images/index.ts
+++ GUI/ETVR/assets/images/index.ts
@@ -5,18 +5,25 @@ import calibrateIcon from './svg/calibrateIcon.svg'
 import cameraSolid from './svg/cameraSolid.svg'
 import croppingIcon from './svg/croppingIcon.svg'
 import defaultCamera from './svg/defaultCamera.svg'
+import eye from './svg/eye.svg'
+import flipIconActive from './svg/flipIconActive.svg'
+import flipIconInactive from './svg/flipIconInactive.svg'
 import gearSolid from './svg/gearSolid.svg'
 import grip from './svg/grip-solid.svg'
 import list from './svg/list-ul-solid.svg'
 import recenterIcon from './svg/recenterIcon.svg'
 import reload from './svg/rotate-right-solid.svg'
+import synchronize from './svg/synchronize.svg'
 
 const icons = {
+    flipIconInactive: flipIconInactive,
+    flipIconActive: flipIconActive,
     calibrateIcon: calibrateIcon,
     defaultCamera: defaultCamera,
     croppingIcon: croppingIcon,
     recenterIcon: recenterIcon,
     cameraSolid: cameraSolid,
+    synchronize: synchronize,
     gearSolid: gearSolid,
     addIcon: addIcon,
     reload: reload,
@@ -24,6 +31,7 @@ const icons = {
     logo: logo,
     list: list,
     grip: grip,
+    eye: eye,
 }
 
 export default icons
