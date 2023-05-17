d212f6455be2887e492b2eb4c8310e25734b8b10
luckmer <woszczo04@gmail.com>
2023-05-17 17:53:54 +0200

    connected crop mode

--- GUI/ETVR/src/pages/Settings/index.tsx
+++ GUI/ETVR/src/pages/Settings/index.tsx
@@ -1,19 +1,43 @@
 import { useNavigate, useParams } from '@solidjs/router'
-import { debug } from 'tauri-plugin-log-api'
+import { CameraCalibrationButtonType } from '@src/static/types/enums'
+import { IBoxPosition } from '@src/static/types/interfaces'
 import { useAppUIContext } from '@src/store/context/ui'
 import Settings from '@src/views/Settings'
 import { useAppCameraContext } from '@store/context/camera'
+import { createSignal } from 'solid-js'
+import { debug } from 'tauri-plugin-log-api'
 
 const SettingsPage = () => {
+    const [isButtonActive, setIsButtonActive] = createSignal({
+        [CameraCalibrationButtonType.CALIBRATE]: false,
+        [CameraCalibrationButtonType.RECENTER]: false,
+        [CameraCalibrationButtonType.CROPPING_MODE]: false,
+    })
+
     const navigate = useNavigate()
     const params = useParams()
 
     const { getSelectedCamera } = useAppCameraContext()
-
     const { setHideHeaderButtons } = useAppUIContext()
+
+    const onClickCroppingMode = (isActive: boolean) => {
+        setIsButtonActive({
+            ...isButtonActive(),
+            [CameraCalibrationButtonType.CROPPING_MODE]: isActive,
+        })
+    }
+
+    const onClickSaveCrop = (boxPosition: IBoxPosition) => {
+        setIsButtonActive({
+            ...isButtonActive(),
+            [CameraCalibrationButtonType.CROPPING_MODE]: false,
+        })
+        debug(`[Crop Settings]: ${JSON.stringify(boxPosition)}`)
+    }
 
     return (
         <Settings
+            isButtonActive={isButtonActive()}
             createNewCamera={params.flag === 'true'}
             camerasUrl={['.', '.', '.']}
             onChange={(value) => debug(value)}
@@ -33,9 +57,8 @@ const SettingsPage = () => {
             onClickRecenter={() => {
                 debug('onClickRecenter')
             }}
-            onClickCroppingMode={() => {
-                navigate('/cropSettings')
-            }}
+            onClickCroppingMode={onClickCroppingMode}
+            onClickSaveCrop={onClickSaveCrop}
             onClick={(selected) => {
                 debug(selected)
             }}
