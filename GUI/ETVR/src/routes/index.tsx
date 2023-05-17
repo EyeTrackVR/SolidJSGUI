f0aa19cc00e2cbd745f5a4b38cb0a3b3411ff6a6
luckmer <woszczo04@gmail.com>
2023-05-17 17:47:53 +0200

    deleted crop page

--- GUI/ETVR/src/routes/index.tsx
+++ GUI/ETVR/src/routes/index.tsx
@@ -4,13 +4,11 @@ import type { RouteDefinition } from '@solidjs/router'
 const Home = lazy(() => import('@pages/Home'))
 const Settings = lazy(() => import('@pages/Settings'))
 const AppSettings = lazy(() => import('@pages/AppSettings'))
-const CropSettings = lazy(() => import('@pages/CropSettings'))
 const page404 = lazy(() => import('@pages/Page404'))
 
 export const routes: RouteDefinition[] = [
     { path: '/', component: Home },
     { path: '/appSettings', component: AppSettings },
-    { path: '/cropSettings', component: CropSettings },
     { path: '/settings/:flag', component: Settings },
     { path: '**', component: page404 },
 ]
